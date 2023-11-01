a73041650fee4115f75295097a90f3f8fce58949
mswed <mswed@beapot.com>
2023-11-01 10:13:52 -0400

    add reverse string

--- 05_reverse_string/reverse_string.py
+++ 05_reverse_string/reverse_string.py
@@ -7,3 +7,7 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+    phrase_as_list = list(phrase)
+    phrase_as_list.reverse()
+
+    return ''.join(phrase_as_list)
