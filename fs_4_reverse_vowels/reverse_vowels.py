f359941663b78f1a110cb253fce2cbf948db6385
mswed <mswed@beapot.com>
2023-11-02 18:24:36 -0400

    add reverse vowels

--- fs_4_reverse_vowels/reverse_vowels.py
+++ fs_4_reverse_vowels/reverse_vowels.py
@@ -1,3 +1,5 @@
+VOWELS = 'aeiouAEIOU'
+
 def reverse_vowels(s):
     """Reverse vowels in a string.
 
@@ -19,3 +21,18 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+
+    sl = list(s)
+    found_vowels = []
+    found_positions = []
+
+    for i, char in enumerate(s):
+        if char in VOWELS:
+            found_vowels.append(char)
+            found_positions.append(i)
+    found_positions.reverse()
+    for p, idx in zip(found_vowels, found_positions):
+        sl[idx] = p
+
+    return ''.join(sl)
+
