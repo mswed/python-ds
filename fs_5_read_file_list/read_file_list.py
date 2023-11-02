5c4ef1b028b847916adfbbdb1fcc8f245836f988
mswed <mswed@beapot.com>
2023-11-02 18:34:45 -0400

    add read file list

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,3 +19,8 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+    with open(filename, 'r') as input_file:
+        for line in input_file.readlines():
+            line = line.rstrip()
+            if line:
+                print('-', line)
