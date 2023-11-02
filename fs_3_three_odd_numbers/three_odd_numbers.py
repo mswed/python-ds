9bf607a237004f2dbe47483fd78ef76eda02c723
mswed <mswed@beapot.com>
2023-11-02 18:08:04 -0400

    add three odd numbers

--- fs_3_three_odd_numbers/three_odd_numbers.py
+++ fs_3_three_odd_numbers/three_odd_numbers.py
@@ -13,3 +13,13 @@ def three_odd_numbers(nums):
         >>> three_odd_numbers([1, 2, 3, 3, 2])
         False
     """
+
+    for i, n in enumerate(nums):
+        try:
+            target = n + nums[i + 1] + nums[i + 2]
+            if target % 2 != 0:
+                return True
+        except IndexError:
+            return False
+
+    return False
